67f6db873f0a0bd97ad6a74211677db54efe10a2
kadridgn01 <projectsartmov@gmail.com>
2025-09-18 13:46:22 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'rota-cache-v1';
+const FILES_TO_CACHE = [
+  '/rota.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/@turf/turf/turf.min.js'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => {
+      if (k !== CACHE_NAME) return caches.delete(k);
+    })))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
